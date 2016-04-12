2b9cc6c79683635ab1c355c0c2854dd4ca1920a7
Brian W <bwalshpdx@gmail.com>
2016-04-12 08:23:11 -0700

    1.1 Exercises Completed

---
+++ brian.w-source/The_C_Programming_Language/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+main()
+{
+
+    printf("<Exercise 1-1>");
+    printf("\n--------------\n");
+
+    printf("hello, world");
+    printf("\n--------------\n");
+
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+
+
+    //Broken version:
+    /*
+    printf("hello, world
+     ");
+
+    */
+
+    printf("\n--------------\n");
+    printf("<Exercise 1-2>");
+    printf("\n--------------\n");
+    printf("hello, \t world");
+    printf("\n--------------\n");
+    printf("hello, \r world");
+    printf("\n--------------\n");
+    printf("hello, \f world");
+}
